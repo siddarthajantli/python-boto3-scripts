130b6f2c6570a46945d86241bcfac65e43ad1728
Siddartha Jantli <39168165+siddarthajantli@users.noreply.github.com>
2023-04-28 11:53:03 +0530

    new scripts

---
+++ list_s3_bucket.py
@@ -0,0 +1,24 @@
+import boto3
+import json
+
+sts_client = boto3.client('sts')
+AWS_ACCOUNT_ID = '<Account_ID>'
+ROLE_NAME = '<Role_Name>'
+
+assumed_role_object=sts_client.assume_role(
+    RoleArn = f'arn:aws:iam::{AWS_ACCOUNT_ID}:role/{ROLE_NAME}',
+    RoleSessionName = "AssumeRoleSession1"
+)
+credentials = assumed_role_object['Credentials']
+
+client = boto3.client(
+    's3',
+    aws_access_key_id = credentials['AccessKeyId'],
+    aws_secret_access_key = credentials['SecretAccessKey'],
+    aws_session_token = credentials['SessionToken']
+)
+
+response = client.list_buckets()
+
+for i in response['Buckets']:
+    print (i['Name'])
